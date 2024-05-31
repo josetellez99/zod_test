a4213e9c72701f5936bf81363736ed264e36d343
Jose Tellez <jdrgv999@gmail.com>
2024-05-31 14:11:48 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-import Image from "next/image";
+'use client'
+
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
   return (
     <div>
       <h1>Hello world</h1>
+      <form 
+        onSubmit={handleSubmit}
+        className='flex flex-col gap-2'
+      >
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} required />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} required />
+        </label>
+        <input 
+          type="submit" 
+          value="Submit" 
+          className='bg-blue-500 max-w-[200px] cursor-pointer'
+          />
+      </form>
     </div>
   );
 }
